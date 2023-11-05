d13174d30a59a1d48a0950487fbff56b865e9c55
rbedard <ryanjbedard@gmail.com>
2023-11-05 09:31:34 -0800

    Add additional code to .js file

--- Assets/script.js
+++ Assets/script.js
@@ -3,7 +3,9 @@ var generateBtn = document.querySelector("#generate");
 var LwrCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var UpprCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var SpclChar ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "!"];
+var SpclCha = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "<", ">", ",", ".", "{", "}", "[", "]"];
+var passwordLength = 12
+
 
 // Write password to the #password input
 function writePassword() {
@@ -12,6 +14,14 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+// function generatePassword() {
+//   var
+
+// }
+// for (var i = 0; i <= passwordLength; i++) {
+//   var
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
